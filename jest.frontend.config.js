ab70990e313d9e43e4b2fc1e503fb57909e1b430
Nicole Lim <limxinyi.nicole@gmail.com>
2025-02-10 20:06:41 +0800

    Fix frontend test configuration

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -1,15 +1,16 @@
 /** @type {import("jest").Config} */
 const config = {
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/**/*.test.{js,jsx}"],
+  collectCoverageFrom: ["client/src/**"],
+  coveragePathIgnorePatterns: ["<rootDir>/client/src/_site/"],
   coverageThreshold: { global: { lines: 100, functions: 100 } },
   displayName: "frontend",
   moduleNameMapper: {
     "\\.(css|scss)$": "identity-obj-proxy",
   },
   testEnvironment: "jest-environment-jsdom",
-  testMatch: ["<rootDir>/client/src/**/*.{spec,test}.{js,jsx}"],
-  testPathIgnorePatterns: ["_site"],
+  testMatch: ["<rootDir>/client/src/**/*.test.js"],
+  testPathIgnorePatterns: ["<rootDir>/client/src/_site/"],
   transform: { "^.+\\.jsx?$": "babel-jest" },
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 };
