7be92deb215faf501c964128c61eecab3bef8670
Nicole Lim <limxinyi.nicole@gmail.com>
2025-02-17 10:55:42 +0800

    Add unit test for creating category

---
+++ controllers/categoryController.test.js
@@ -0,0 +1,91 @@
+import { expect, jest } from "@jest/globals";
+import categoryModel from "../models/categoryModel";
+import { createCategoryController } from "./categoryController";
+
+describe("Category controller", () => {
+  describe("Create category controller", () => {
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("Allow creating of unique category", async () => {
+      const req = { body: { name: "test" } };
+      const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+
+      categoryModel.findOne = jest.fn().mockResolvedValue(null);
+      categoryModel.prototype.save = jest
+        .fn()
+        .mockResolvedValue({ name: "test", slug: "test" });
+
+      await createCategoryController(req, res);
+
+      expect(categoryModel.findOne).toHaveBeenCalledWith({ name: "test" });
+      expect(categoryModel.prototype.save).toHaveBeenCalledTimes(1);
+      expect(res.status).toHaveBeenCalledWith(201);
+      expect(res.send).toHaveBeenCalledWith({
+        success: true,
+        message: "new category created",
+        category: { name: "test", slug: "test" },
+      });
+    });
+
+    it("Should not allow creating of duplicate category", async () => {
+      const req = { body: { name: "test" } };
+      const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+
+      categoryModel.findOne = jest
+        .fn()
+        .mockResolvedValue({ name: "test", slug: "test" });
+
+      await createCategoryController(req, res);
+
+      expect(categoryModel.findOne).toHaveBeenCalledWith({ name: "test" });
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.send).toHaveBeenCalledWith({
+        success: true,
+        message: "Category Already Exisits",
+      });
+      expect(categoryModel.prototype.save).not.toHaveBeenCalled();
+    });
+
+    it("Should not allow creating of category without name", async () => {
+      const req = { body: {} };
+      const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+
+      await createCategoryController(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(401);
+      expect(res.send).toHaveBeenCalledWith({ message: "Name is required" });
+      expect(categoryModel.prototype.save).not.toHaveBeenCalled();
+    });
+
+    it("Should not allow creating of category when name contains whitespaces only", async () => {
+      const req = { body: { name: " " } };
+      const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+
+      await createCategoryController(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(401);
+      expect(res.send).toHaveBeenCalledWith({ message: "Name is required" });
+      expect(categoryModel.prototype.save).not.toHaveBeenCalled();
+    });
+
+    it("Should handle error when creating category", async () => {
+      const req = { body: { name: "test" } };
+      const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+
+      const mockError = new Error("Error creating category");
+      categoryModel.findOne = jest.fn().mockResolvedValue(null);
+      categoryModel.prototype.save = jest.fn().mockRejectedValue(mockError);
+
+      await createCategoryController(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.send).toHaveBeenCalledWith({
+        success: false,
+        errro: mockError,
+        message: "Errro in Category",
+      });
+    });
+  });
+});
