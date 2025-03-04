e02b254cb733463f77e9b7b23ae8d2550c28246e
choo tze jie <119966583+tjch-o@users.noreply.github.com>
2025-03-04 21:06:18 +0800

    Add unit tests for policy page

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -4,22 +4,21 @@ import Layout from "./../components/Layout";
 const Policy = () => {
   return (
     <Layout title={"Privacy Policy"}>
-      <div className="row contactus ">
-        <div className="col-md-6 ">
-          <img
-            src="/images/contactus.jpeg"
-            alt="contactus"
-            style={{ width: "100%" }}
-          />
+      <div className='row contactus'>
+        <div className='col-md-6 '>
+          <img src='/images/contactus.jpeg' alt='Privacy Policy' style={{ width: "100%" }} />
         </div>
-        <div className="col-md-4">
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
+        <div className='col-md-4'>
+          <h1 className='bg-dark p-2 text-white text-center'>PRIVACY POLICY</h1>
+          <div data-testid='policy-content'>
+            <p>add privacy policy</p>
+            <p>add privacy policy</p>
+            <p>add privacy policy</p>
+            <p>add privacy policy</p>
+            <p>add privacy policy</p>
+            <p>add privacy policy</p>
+            <p>add privacy policy</p>
+          </div>
         </div>
       </div>
     </Layout>
