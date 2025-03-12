621552a6eddfbf46becaf78348a9645467f0367a
Nicole Lim <limxinyi.nicole@gmail.com>
2025-03-12 10:58:58 +0800

    Update integration tests

--- routes/productRoutes.test.js
+++ routes/productRoutes.test.js
@@ -11,17 +11,27 @@ describe("Product Routes", () => {
   describe("GET /api/v1/product/search", () => {
     beforeEach(async () => {
       await productModel.deleteMany({});
-      await productModel.create({
-        name: "phone",
-        slug: "phone",
-        description: "phone description",
-        price: 1000,
-        category: new mongoose.Types.ObjectId("66db427fdb0119d9234b27ee"),
-        quantity: 10,
-      });
+      await productModel.create([
+        {
+          name: "phone",
+          slug: "phone",
+          description: "phone description",
+          price: 1000,
+          category: new mongoose.Types.ObjectId("66db427fdb0119d9234b27ee"),
+          quantity: 10,
+        },
+        {
+          name: "laptop",
+          slug: "laptop",
+          description: "laptop description",
+          price: 2000,
+          category: new mongoose.Types.ObjectId("66db427fdb0119d9234b27ee"),
+          quantity: 20,
+        },
+      ]);
     });
 
-    it("Should return all products", async () => {
+    it("Should return a list of products containing matching keyword", async () => {
       const keyword = "phone";
       const response = await request(app).get(
         `/api/v1/product/search/${keyword}`
@@ -37,6 +47,16 @@ describe("Product Routes", () => {
       expect(response.body[0]).toHaveProperty("price", 1000);
       expect(response.body[0]).toHaveProperty("category");
       expect(response.body[0]).toHaveProperty("quantity", 10);
+    });
+
+    it("Should return an empty list when keyword does not match products", async () => {
+      const keyword = "non-existent";
+      const response = await request(app).get(
+        `/api/v1/product/search/${keyword}`
+      );
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual([]);
     });
   });
 });
