0362f29c7a376966b0cb21036755225ee5fe3b12
choo tze jie <119966583+tjch-o@users.noreply.github.com>
2025-03-12 21:46:31 +0800

    Add integration test for getProduct API and database

--- routes/productRoutes.test.js
+++ routes/productRoutes.test.js
@@ -3,7 +3,7 @@ import productRoutes from "./productRoutes";
 import request from "supertest";
 import productModel from "../models/productModel";
 import mongoose from "mongoose";
-import { beforeAll, afterAll } from "@jest/globals";
+import { beforeAll, afterAll, expect } from "@jest/globals";
 import { MongoMemoryServer } from "mongodb-memory-server";
 
 const app = Express();
@@ -53,17 +53,12 @@ describe("Product Routes", () => {
 
     it("Should return a list of products containing matching keyword", async () => {
       const keyword = "phone";
-      const response = await request(app).get(
-        `/api/v1/product/search/${keyword}`
-      );
+      const response = await request(app).get(`/api/v1/product/search/${keyword}`);
       expect(response.status).toBe(200);
       expect(response.body).toHaveLength(1);
       expect(response.body[0]).toHaveProperty("name", "phone");
       expect(response.body[0]).toHaveProperty("slug", "phone");
-      expect(response.body[0]).toHaveProperty(
-        "description",
-        "phone description"
-      );
+      expect(response.body[0]).toHaveProperty("description", "phone description");
       expect(response.body[0]).toHaveProperty("price", 1000);
       expect(response.body[0]).toHaveProperty("category");
       expect(response.body[0]).toHaveProperty("quantity", 10);
@@ -71,12 +66,66 @@ describe("Product Routes", () => {
 
     it("Should return an empty list when keyword does not match products", async () => {
       const keyword = "non-existent";
-      const response = await request(app).get(
-        `/api/v1/product/search/${keyword}`
-      );
+      const response = await request(app).get(`/api/v1/product/search/${keyword}`);
 
       expect(response.status).toBe(200);
       expect(response.body).toEqual([]);
+    });
+  });
+
+  describe("GET /api/v1/product/get-product", () => {
+    beforeEach(async () => {
+      await productModel.deleteMany({});
+      await productModel.create([
+        {
+          name: "book",
+          slug: "book",
+          description: "book description",
+          price: 10,
+          category: new mongoose.Types.ObjectId("67d18a47b92dddc71c78f644"),
+          quantity: 20,
+        },
+        {
+          name: "laptop",
+          slug: "laptop",
+          description: "laptop description",
+          price: 2000,
+          category: new mongoose.Types.ObjectId("67d18a5fe08b3e56cc31552c"),
+          quantity: 25,
+        },
+      ]);
+    });
+
+    it("Should return product with the given slug", async () => {
+      const slug = "book";
+      const response = await request(app).get(`/api/v1/product/get-product/${slug}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("message", "Single Product Fetched");
+      expect(response.body).toHaveProperty("success", true);
+      expect(response.body).toHaveProperty("product");
+
+      const { product } = response.body;
+
+      expect(product).toHaveProperty("name", "book");
+      expect(product).toHaveProperty("slug", "book");
+      expect(product).toHaveProperty("description", "book description");
+      expect(product).toHaveProperty("price", 10);
+      expect(product).toHaveProperty("category");
+      expect(product).toHaveProperty("quantity", 20);
+    });
+
+    it("Should return no product if slug does not exist", async () => {
+      const slug = "camera";
+      const response = await request(app).get(`/api/v1/product/get-product/${slug}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("message", "Single Product Fetched");
+      expect(response.body).toHaveProperty("success", true);
+      expect(response.body).toHaveProperty("product");
+
+      const { product } = response.body;
+      expect(product).toBeNull();
     });
   });
 });
