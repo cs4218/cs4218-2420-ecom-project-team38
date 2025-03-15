43b413d20cc629e35adb27867d9513190fdf31df
Tin Ruiqi <tinruiqi2002@gmail.com>
2025-03-15 17:11:38 +0800

    Write integration tests for get orders api

--- routes/authRoutes.integration.test.js
+++ routes/authRoutes.integration.test.js
@@ -2,6 +2,8 @@ import express from "express";
 import authRoutes from "./authRoutes";
 import request from "supertest";
 import userModel from "../models/userModel";
+import productModel from "../models/productModel";
+import orderModel from "../models/orderModel";
 import mongoose from "mongoose";
 import { beforeAll, afterAll, jest } from "@jest/globals";
 import { MongoMemoryServer } from "mongodb-memory-server";
@@ -181,6 +183,143 @@ describe("Auth Routes", () => {
       expect(response.body).toHaveProperty(
         "message",
         "Error while updating profile"
+      );
+      expect(response.body).toHaveProperty("error");
+
+      const mongoUri = mongo.getUri();
+      await mongoose.connect(mongoUri);
+    });
+  });
+
+  describe("GET /api/v1/auth/orders", () => {
+    const GET_ORDERS_API = "/api/v1/auth/orders";
+    process.env.JWT_SECRET = "test-secret";
+
+    const mockUser = {
+      _id: new mongoose.Types.ObjectId("65d21b4667d0d8992e610c85"),
+      name: "Test User",
+      email: "testuser@gmail.com",
+      password: "testpassword123",
+      phone: "98765432",
+      address: "21 Lower Kent Ridge Rd",
+      answer: "Software Testing",
+      DOB: new Date("01/23/2004"),
+    };
+    const mockOtherUser = {
+      _id: new mongoose.Types.ObjectId("6714c2a30e8ea8335e4104ff"),
+      name: "Other User",
+      email: "otheruser@gmail.com",
+      password: "otherpassword123",
+      phone: "87654321",
+      address: "13 Computing Drive",
+      answer: "Software Development",
+      DOB: new Date("12/09/2002"),
+    };
+
+    const mockProduct = {
+      _id: new mongoose.Types.ObjectId("66db427fdb0119d9234b27f3"),
+      name: "Test Product Name",
+      slug: "Test-Product-Name",
+      description: "Test Product Description",
+      price: 4.99,
+      category: new mongoose.Types.ObjectId("66db427fdb0119d9234b27ef"),
+      quantity: 20,
+      photo: {},
+      shipping: false,
+    };
+
+    const mockUserOrder = {
+      _id: new mongoose.Types.ObjectId("67a1e2345c9d8f8b9e4b3c1a"),
+      status: "Not Processed",
+      buyer: mockUser._id,
+      payment: { success: true },
+      products: [mockProduct._id],
+    };
+    const mockOtherUserOrder = {
+      _id: new mongoose.Types.ObjectId("67b2f5356aeeaf5676d72a44"),
+      status: "Shipped",
+      buyer: mockOtherUser._id,
+      payment: { success: false },
+      products: [mockProduct._id],
+    };
+
+    let mockToken;
+
+    beforeEach(async () => {
+      await userModel.deleteMany({});
+      await userModel.create([mockUser, mockOtherUser]);
+      mockToken = JWT.sign({ _id: mockUser._id }, process.env.JWT_SECRET);
+
+      await productModel.deleteMany({});
+      await productModel.create(mockProduct);
+
+      await orderModel.deleteMany({});
+      await orderModel.create([mockUserOrder, mockOtherUserOrder]);
+    });
+
+    it("should return successful response with order details when get orders is successful", async () => {
+      const response = await request(app)
+        .get(GET_ORDERS_API)
+        .set("Authorization", mockToken);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveLength(1);
+      expect(response.body[0]).toHaveProperty(
+        "_id",
+        mockUserOrder._id.toString()
+      );
+      expect(response.body[0]).toHaveProperty("status", mockUserOrder.status);
+      expect(response.body[0].buyer).toHaveProperty("name", mockUser.name);
+      expect(response.body[0]).toHaveProperty("createdAt");
+      expect(response.body[0].payment).toHaveProperty("success", true);
+      expect(response.body[0].products).toHaveLength(1);
+    });
+
+    it("should return successful response with product details when get orders is successful", async () => {
+      const response = await request(app)
+        .get(GET_ORDERS_API)
+        .set("Authorization", mockToken);
+
+      expect(response.status).toBe(200);
+      expect(response.body[0].products[0]).toHaveProperty(
+        "_id",
+        mockProduct._id.toString()
+      );
+      expect(response.body[0].products[0]).toHaveProperty(
+        "name",
+        mockProduct.name
+      );
+      expect(response.body[0].products[0]).toHaveProperty(
+        "description",
+        mockProduct.description
+      );
+      expect(response.body[0].products[0]).toHaveProperty(
+        "price",
+        mockProduct.price
+      );
+      expect(response.body[0].products[0]).not.toHaveProperty("photo");
+    });
+
+    it("should return error response when request is unauthorized", async () => {
+      const response = await request(app).get(GET_ORDERS_API);
+
+      expect(response.status).toBe(401);
+      expect(response.body).toHaveProperty("success", false);
+      expect(response.body).toHaveProperty("message", "Unauthorized Access");
+    });
+
+    it("should return error response when there is a database error", async () => {
+      await mongoose.disconnect();
+
+      const response = await request(app)
+        .get(GET_ORDERS_API)
+        .set("Authorization", mockToken);
+
+      expect(response.status).toBe(500);
+      expect(response.body).toHaveProperty("success", false);
+      expect(response.body).toHaveProperty(
+        "message",
+        "Error while getting orders"
       );
       expect(response.body).toHaveProperty("error");
 
