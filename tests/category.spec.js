10d918cee5bb467b357d738898c6d9f03cdddfcc
choo tze jie <119966583+tjch-o@users.noreply.github.com>
2025-03-21 21:28:25 +0800

    Remove authenticated test case for now

--- tests/category.spec.js
+++ tests/category.spec.js
@@ -46,7 +46,7 @@ async function logoutIfLoggedIn(page) {
   }
 }
 
-test.describe("Category Page - Unauthenticated Users", () => {
+test.describe("Category Page", () => {
   test.beforeEach(async ({ page }) => {
     await logoutIfLoggedIn(page);
 
@@ -91,67 +91,5 @@ test.describe("Category Page - Unauthenticated Users", () => {
 
     await expect(page.getByText("You have 1 item in your cart")).toBeVisible();
     await expect(page.getByRole("button", { name: "Please login to checkout" })).toBeVisible();
-  });
-});
-
-test.describe("Category Page - Authenticated Users", () => {
-  const testAccount = {
-    email: "DomUITest@gmail.com",
-    name: "DomUITest",
-    password: "Password123!",
-    address: "Test Address",
-  };
-
-  test.beforeEach(async ({ page }) => {
-    await logoutIfLoggedIn(page);
-
-    const mongoUri = process.env.MONGO_URL;
-    await mongoose.connect(mongoUri);
-    await categoryModel.create(categories);
-    await productModel.create(products);
-
-    await page.goto("/login");
-    await page.getByRole("textbox", { name: "Enter Your Email" }).fill(testAccount.email);
-    await page.getByRole("textbox", { name: "Enter Your Password" }).fill(testAccount.password);
-    await page.getByTestId("login-button").click();
-    await page.goto("/");
-  });
-
-  test.afterEach(async () => {
-    await productModel.deleteMany({});
-    await categoryModel.deleteMany({});
-    await mongoose.disconnect();
-  });
-
-  test("should navigate through categories, select a category, add a product in that category to the cart, and show make payment", async ({
-    page,
-  }) => {
-    await page.click("text=Categories");
-
-    await expect(page.locator(".dropdown-menu").first()).toBeVisible();
-    await expect(page.locator(".dropdown-menu").first()).toContainText("All Categories");
-    await expect(page.locator(".dropdown-menu")).toContainText("Electronics");
-
-    await page.click("text=Electronics");
-    await page.waitForURL("/category/electronics");
-    await page.getByRole("button", { name: "More Details" }).nth(1).click();
-
-    await expect(page.getByRole("heading", { name: `Name : ${products[1].name}` })).toBeVisible();
-    await expect(
-      page.getByRole("heading", { name: `Description : ${products[1].description}` })
-    ).toBeVisible();
-    await expect(
-      page.getByRole("heading", { name: `Category : ${categories[0].name}` })
-    ).toBeVisible();
-
-    await page.getByRole("button", { name: "ADD TO CART" }).first().click();
-
-    await page.getByRole("link", { name: "Cart" }).click();
-    await page.waitForURL("/cart");
-
-    await expect(page.getByText("You have 1 item in your cart")).toBeVisible();
-    await expect(page.getByRole("heading", { name: `${testAccount.address}` })).toBeVisible();
-    await expect(page.getByRole("button", { name: "Update Address" })).toBeVisible();
-    await expect(page.getByRole("button", { name: "Make Payment" })).toBeVisible();
   });
 });
